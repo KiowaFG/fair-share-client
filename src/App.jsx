68432f9679c0d63b97f98ae4778f9519fb0f4bee
nisol <nisolmedinaue@gmail.com>
2024-05-30 06:25:30 +0200

    landingPage-init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header'
 import Homepage from './Pages/HomePage'
 import SideBar from './Components/SideBar'
+import LandingPage from './Pages/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Homepage/>
+    <Routes>
+      <Route path='/' element= {<LandingPage/>}/>
+      <Route path='/HomePage' element={<Homepage/>}/>
+    </Routes>
+      
     </>
   )
 }

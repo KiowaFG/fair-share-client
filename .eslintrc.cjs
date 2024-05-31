ecad265680443b35d90da2e303b8fad05c28ff12
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-31 12:33:47 +0200

    updating linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'semicolon': [true, 'always', 'ignore-bound-class-methods'],
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',

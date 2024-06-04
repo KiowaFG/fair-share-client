4ef6cb87033b18c023e587d4cf86bf01d260b442
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-06-04 18:12:59 +0200

    details added and remove expense functionality

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,21 +1,16 @@
 import "../App.css"
 import "./HomePage.css"
-import { Link } from "react-router-dom"
-import Header from "../Components/Header"
-import SideBar from "../Components/SideBar"
 import Overview from "../Components/Overview"
 import Expenses from "../Components/Expenses"
 
 
 function Homepage() {
     return (
-        <>
-                
+        <>     
                 <div className="overviewExpenses">
                     <Overview />
                     <Expenses />
                 </div>
-            
         </>
     )
 }

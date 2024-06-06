1b6cbc2f466b1a5895f489f28c577735768bd251
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-06-06 16:56:54 +0200

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProviderWrapper } from "./context/auth.context.jsx"
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <AuthProviderWrapper>
         <App />
       </ AuthProviderWrapper>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
